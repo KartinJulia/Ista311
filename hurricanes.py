3525abbe1ad29cf3af142c14fb60132bed15dbc0
KartinWWW <31469746+KartinJulia@users.noreply.github.com>
2019-11-25 13:54:10 -0700

    upd

--- hurricanes.py
+++ hurricanes.py
@@ -5,7 +5,12 @@ from scipy import stats
 class HurricaneModel(InferenceSuite):
     def likelihood(self, data, hypothesis):
          p = sp.stats.poisson.pmf(data, hypothesis)
+         return p
         #sp.stats.poisson.rvs(rate)
 
     def predict(self):
-        return sp.stats.poisson.rvs(sample())
+        return sp.stats.poisson.rvs(self.sample())
+
+data = sp.loadtxt('hurricanes.csv', delimiter = ',', skiprows = 1)
+#print(data)
+i1 = HurricaneModel(data)
